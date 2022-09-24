791165eb2ff1b6566ea284380477db7d1a16ff0f
Jose Miguel Millán Londoño <73767449+jevo160296@users.noreply.github.com>
2022-09-24 18:43:00 -0500

    Corrección errores en LimpiezaCalidad y creación de unittests.

--- src/data/procesamiento_datos.py
+++ src/data/procesamiento_datos.py
@@ -2,7 +2,7 @@ from typing import Union, List
 
 from pandas import DataFrame
 from sklearn.base import BaseEstimator, TransformerMixin
-from sklearn.pipeline import make_pipeline, Pipeline
+from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import FunctionTransformer
 
 from src.data.funciones_base import convertir_tipos, eliminar_duplicados, convertir_col_date_a_date, \
@@ -12,31 +12,33 @@ from src.data.funciones_base import convertir_tipos, eliminar_duplicados, conver
 class LimpiezaCalidad(BaseEstimator, TransformerMixin):
     def __init__(self, columnas_numericas: List[str]):
         self._pipeline: Union[None, Pipeline] = None
-        self._columnas_numericas: List[str] = columnas_numericas
+        self.columnas_numericas: List[str] = columnas_numericas
 
     @property
     def pipeline(self) -> Pipeline:
         if self._pipeline is None:
-            self._pipeline = make_pipeline([
-                FunctionTransformer(self._convertir_tipos),
-                FunctionTransformer(eliminar_duplicados),
-                FunctionTransformer(convertir_col_date_a_date),
-                FunctionTransformer(self._reemplazar_valores_extremos),
-                FunctionTransformer(self._reemplazar_nulos_por_la_media),
-                FunctionTransformer(reemplazar_fechas_nulas),
-                FunctionTransformer(reemplazar_ceros_por_nulos),
-                FunctionTransformer(eliminar_duplicados)
-            ])
+            self._pipeline = Pipeline([
+                ('convertir_tipos', FunctionTransformer(self._convertir_tipos)),
+                ('eliminar_duplicados', FunctionTransformer(eliminar_duplicados)),
+                ('col_date_a_date', FunctionTransformer(convertir_col_date_a_date)),
+                ('valores_extremos', FunctionTransformer(self._reemplazar_valores_extremos)),
+                ('nulos_por_media', FunctionTransformer(self._reemplazar_nulos_por_la_media)),
+                ('fechas_nulas', FunctionTransformer(reemplazar_fechas_nulas)),
+                ('ceros_por_nulos', FunctionTransformer(reemplazar_ceros_por_nulos)),
+                ('eliminar_duplicados2', FunctionTransformer(eliminar_duplicados)),
+                ('passtrhough', None)
+            ]
+            )
         return self._pipeline
 
     def _convertir_tipos(self, df: DataFrame) -> DataFrame:
-        return convertir_tipos(df, self._columnas_numericas)
+        return convertir_tipos(df, self.columnas_numericas)
 
     def _reemplazar_valores_extremos(self, df: DataFrame) -> DataFrame:
-        return reemplazar_valores_extremos(df, self._columnas_numericas)
+        return reemplazar_valores_extremos(df, self.columnas_numericas)
 
     def _reemplazar_nulos_por_la_media(self, df: DataFrame) -> DataFrame:
-        return reemplazar_nulos_por_la_media(df, self._columnas_numericas)
+        return reemplazar_nulos_por_la_media(df, self.columnas_numericas)
 
     def fit(self, X: DataFrame, y=None):
         self.pipeline.fit(X)
