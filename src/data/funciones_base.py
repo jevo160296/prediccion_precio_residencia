a34280e0b6e1a8e3be0f5ea4bec145d5f99e737f
Jose Miguel Millán Londoño <73767449+jevo160296@users.noreply.github.com>
2022-09-24 16:35:11 -0500

    Se crean funciones para realizar una limpieza de calidad de datos. - convertir_col_date_a_date - reemplazar_valores_extremos - reemplazar_extremos_por_la_media - reemplazar_fechas_nulas - reemplazar_ceros_por_nulos

--- src/data/funciones_base.py
+++ src/data/funciones_base.py
@@ -3,6 +3,8 @@ from typing import Tuple, Callable
 from pandas import DataFrame
 from sklearn.model_selection import train_test_split
 from pathlib import Path
+import pandas as pd
+import numpy as np
 
 
 def validar_duplicados(df: DataFrame):
@@ -18,6 +20,45 @@ def eliminar_duplicados(df: DataFrame):
     filas = df.shape[0]
     print(f'Después de la eliminación de duplicados, el conjunto de datos queda con {filas} filas.')
     return df
+
+
+def convertir_col_date_a_date(df: DataFrame) -> DataFrame:
+    _df = df.copy()
+    _df['date'] = pd.to_datetime(_df['date'], errors='coerce')
+    return _df
+
+
+def reemplazar_valores_extremos(df: DataFrame, columnas_numericas) -> DataFrame:
+    _df = df.copy()
+    _df[columnas_numericas] = _df[columnas_numericas].where(lambda x: x > -1e+10, other=np.nan).where(
+        lambda x: x < 1e+10, other=np.nan)
+    return _df
+
+
+def reemplazar_extremos_por_la_media(df: DataFrame, columnas_numericas) -> DataFrame:
+    # Se reemplazan los valores extremos por la media Nota: No se considera que haya data leakage pues los valores
+    # reemplazados son entre registros con el mismo index y como al final se va a dejar un dataset con index únicos,
+    # no hay riesgo que estén tanto en el set de entrenamiento como en el de test
+    _df = df.copy()
+    for columna_numerica in columnas_numericas:
+        _df[columna_numerica] = _df[columna_numerica].fillna(
+            _df.groupby('index')[columna_numerica].transform('median'))
+    return _df
+
+
+def reemplazar_fechas_nulas(df: DataFrame) -> DataFrame:
+    _df = df.copy()
+    # Reemplazando fechas nulas por la primera fecha no nula
+    _df['date'] = _df['date'].fillna(
+        _df.groupby(['index'], sort=False)['date'].apply(lambda x: x.ffill().bfill()))
+    return _df
+
+
+def reemplazar_ceros_por_nulos(df: DataFrame) -> DataFrame:
+    _df = df.copy()
+    # Reemplazando ceros por valores nulos
+    _df[['sqft_basement', 'yr_renovated']] = _df[['sqft_basement', 'yr_renovated']].replace(0, np.nan)
+    return _df
 
 
 def validar_index_duplicados(df: DataFrame):
