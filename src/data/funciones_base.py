19a23de8122363c93b089d90f394cd6a4ddb338f
Jose Miguel Millán Londoño <73767449+jevo160296@users.noreply.github.com>
2022-09-24 11:03:23 -0500

    Cambio en variables.

--- src/data/funciones_base.py
+++ src/data/funciones_base.py
@@ -5,20 +5,20 @@ def validar_duplicados(df):
         f'De {filas} registros hay {cant_duplicados} filas duplicadas, representando el {cant_duplicados / filas:.2%}')
 
 
-def eliminar_duplicados(_df):
+def eliminar_duplicados(df):
     # Eliminando duplicados
-    df = _df.drop_duplicates(keep='first')
-    filas = _df.shape[0]
+    df = df.drop_duplicates(keep='first')
+    filas = df.shape[0]
     print(f'Después de la eliminación de duplicados, el conjunto de datos queda con {filas} filas.')
     return df
 
 
-def validar_index_duplicados(_df):
+def validar_index_duplicados(df):
     # Validando duplicados de index
-    _son_duplicados = _df['index'].duplicated()
-    _cant_duplicados = _son_duplicados.sum()
-    _filas = _df.shape[0]
+    son_duplicados = df['index'].duplicated()
+    cant_duplicados = son_duplicados.sum()
+    filas = df.shape[0]
     print(
-        f'De {_filas} registros, hay {_cant_duplicados} registros con index duplicado, que representan el '
-        f'{_cant_duplicados / _filas:.2%}.')
-    return _son_duplicados
+        f'De {filas} registros, hay {cant_duplicados} registros con index duplicado, que representan el '
+        f'{cant_duplicados / filas:.2%}.')
+    return son_duplicados
