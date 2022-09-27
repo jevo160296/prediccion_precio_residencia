1525e813ce742f52cb5e9c82094d61f985beb586
Jose Miguel Millán Londoño <73767449+jevo160296@users.noreply.github.com>
2022-09-26 20:38:16 -0500

    Se crean funciones para realizar pruebas al pipeline de procesamiento de datos.

--- src/data/procesamiento_datos.py
+++ src/data/procesamiento_datos.py
@@ -2,11 +2,18 @@ from typing import Union, List
 
 from pandas import DataFrame
 from sklearn.base import BaseEstimator, TransformerMixin
-from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import FunctionTransformer
+from sklearn.compose import make_column_transformer
+from sklearn.pipeline import Pipeline, make_union
+from sklearn.preprocessing import FunctionTransformer, PowerTransformer, KBinsDiscretizer
 
 from src.data.funciones_base import convertir_tipos, eliminar_duplicados, convertir_col_date_a_date, \
     reemplazar_valores_extremos, reemplazar_nulos_por_la_media, reemplazar_fechas_nulas, reemplazar_ceros_por_nulos
+
+from src.features.procesamiento_datos import transformacion_logaritmica_y, entrenar_logaritmica, entrenar_logaritmica_y, \
+    transformacion_logaritmica, numericas_a_binarias
+
+from src.features.limpiezaDatos1 import conversionTipoDatos, eliminacionColumnas, eliminacionOutliers, \
+    calculoVariablesAdicionales
 
 
 class LimpiezaCalidad(BaseEstimator, TransformerMixin):
@@ -39,6 +46,41 @@ class LimpiezaCalidad(BaseEstimator, TransformerMixin):
 
     def _reemplazar_nulos_por_la_media(self, df: DataFrame) -> DataFrame:
         return reemplazar_nulos_por_la_media(df, self.columnas_numericas)
+
+    def fit(self, X: DataFrame, y=None):
+        self.pipeline.fit(X)
+
+    def transform(self, X: DataFrame, y=None) -> DataFrame:
+        return self.pipeline.transform(X)
+
+
+class ProcesamientoDatos(BaseEstimator, TransformerMixin):
+    def __init__(self):
+        self._pipeline: Union[None, Pipeline] = None
+
+    @property
+    def pipeline(self) -> Pipeline:
+        if self._pipeline is None:
+            self._pipeline = Pipeline([
+                ('eliminar_outliers', FunctionTransformer(eliminacionOutliers)),
+                ('conversion_tipos', FunctionTransformer(conversionTipoDatos)),
+                ('calculo_variables_adicionales', FunctionTransformer(calculoVariablesAdicionales)),
+                ('eliminacion_columnas', FunctionTransformer(eliminacionColumnas)),
+                ('numericas_a_binarias', FunctionTransformer(numericas_a_binarias)),
+                ('Logaritmica', make_union(
+                        make_column_transformer(
+                            (PowerTransformer(method='box-cox'),
+                             ['sqft_above', 'sqft_living15', 'sqft_lot', 'sqft_lot15', 'sqft_living']),
+                            (KBinsDiscretizer(strategy='kmeans', encode='ordinal'),
+                             ['sqft_lot', 'sqft_lot15'])
+                        ),
+                        make_column_transformer(
+                            ('drop', ['sqft_above', 'sqft_living15', 'sqft_lot', 'sqft_lot15', 'sqft_living']))
+                )),
+                ('passtrhough', None)
+            ]
+            )
+        return self._pipeline
 
     def fit(self, X: DataFrame, y=None):
         self.pipeline.fit(X)
--- src/features/limpiezaDatos1.py
+++ src/features/limpiezaDatos1.py
@@ -1,18 +1,7 @@
-
-
 import pandas as pd
-import numpy as np
-import logging
-from pathlib import Path
 
 
-
-def main(input_filepath, output_filepath):
-    """ Runs data feature engineering scripts to turn interim data from (../interim) into
-        cleaned data ready for machine learning (saved in ../processed).
-    """
-
-def eliminacionOutliers(data:pd.DataFrame)->pd.DataFrame:
+def eliminacionOutliers(data: pd.DataFrame) -> pd.DataFrame:
     data = data[data['price'] <= 1130000]
     data = data[data['sqft_lot'] <= 350000]
     data = data[data['bedrooms'] <= 5]
@@ -23,14 +12,14 @@ def eliminacionOutliers(data:pd.DataFrame)->pd.DataFrame:
     return data
 
 
-
-def conversionTipoDatos(data:pd.DataFrame)->pd.DataFrame:
+def conversionTipoDatos(data: pd.DataFrame) -> pd.DataFrame:
     variables_categoricas = ['grade', 'view', 'waterfront', 'condition', 'zipcode']
     data[variables_categoricas] = data[variables_categoricas].astype('category')
 
     return data
 
-def calculoVariablesAdicionales(data: pd.DataFrame)->pd.DataFrame:
+
+def calculoVariablesAdicionales(data: pd.DataFrame) -> pd.DataFrame:
     data['yr_date'] = data['date'].dt.year
     data['antiguedad_venta'] = data['yr_date'] - data['yr_built']
     data.drop(columns=['yr_date', 'date', 'yr_built'], inplace=True)
@@ -38,17 +27,7 @@ def calculoVariablesAdicionales(data: pd.DataFrame)->pd.DataFrame:
     return data
 
 
-def eliminacionColumnas(data: pd.DataFrame)->pd.DataFrame:
+def eliminacionColumnas(data: pd.DataFrame) -> pd.DataFrame:
     data.drop(columns=['lat', 'yr_renovated', 'long', 'jhygtf'], inplace=True)
 
     return data
-
-
-
-
-
-
-
-
-
-
